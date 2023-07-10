3d6af169de48de8371929b67d1034773c5eb5e5a
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2023-07-10 17:10:55 +0200

    type check: times

--- explore.R
+++ explore.R
@@ -3,9 +3,32 @@ library(tidyverse)
 compile_times <- read_csv("data/node_compile_times.csv")
 compile_times
 
+# columns
+str(compile_times)
+glimpse(compile_times)
+
+# time type
+class(compile_times$time)
+
 table(compile_times$vendor)
 table(compile_times$model)
 table(compile_times$cpu)
 
 compile_times |>
   filter(vendor == "Apple")
+
+compile_times |> 
+  filter(model %in% c("Elitebook 840 G1", "Elitebook 840 G3"))
+
+
+
+
+## Wrangling
+
+# convert time (min:sec) to seconds
+
+
+
+# add new t_sec column
+compile_times |> 
+  mutate(t_sec = )
